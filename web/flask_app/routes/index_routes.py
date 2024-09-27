edac030e4b503f73ec7b535f93ba847890271977
toann1980 <28768308+toann1980@users.noreply.github.com>
2024-09-27 09:41:11 -0700

    Refactor: Remove unused routes and templates in index_routes.py

--- web/flask_app/routes/index_routes.py
+++ web/flask_app/routes/index_routes.py
@@ -1,37 +1,24 @@
-from flask import (
-    Blueprint, jsonify, redirect, render_template, request, url_for
-)
-from user_agents import parse
+from flask import (Blueprint, jsonify, render_template)
 
 from choreboss.schemas.chore_schema import ChoreSchema
 from choreboss.schemas.people_schema import PeopleSchema
 from web.flask_app.routes.chore_routes import chore_service
 from web.flask_app.routes.people_routes import people_service
 
+
 index_bp = Blueprint('index_bp', __name__)
 chore_schema = ChoreSchema(many=True)
 people_schema = PeopleSchema(many=True)
-
-device_pattern = r'\(([^)]+)\)'
 
 
 @index_bp.route('/', methods=['GET'])
 def home():
     people = people_service.get_all_people()
+    print(f'people: {people}')
     chores = chore_service.get_all_chores()
+    print(f'chores: {chores}')
 
-    people_json = people_schema.dump(people)
-    chores_json = chore_schema.dump(chores)
-
-    user_agent_string = request.headers.get('User-Agent')
-    user_agent = parse(user_agent_string)
-
-    if user_agent.is_mobile:
-        return redirect(url_for('index_bp.mobile_home'))
-    elif user_agent.is_tablet:
-        return redirect(url_for('index_bp.tablet_home'))
-    else:
-        return redirect(url_for('index_bp.desktop_home'))
+    return render_template('index.html', people=people, chores=chores)
 
 
 @index_bp.route('/mobile', methods=['GET'])
@@ -42,13 +29,3 @@ def mobile_home():
 @index_bp.route('/tablet', methods=['GET'])
 def tablet_home():
     return jsonify({'message': 'Welcome to the tablet site!'})
-
-
-@index_bp.route('/desktop', methods=['GET'])
-def desktop_home():
-    people = people_service.get_all_people()
-    print(f'people: {people}')
-    chores = chore_service.get_all_chores()
-    print(f'chores: {chores}')
-
-    return render_template('desktop_home.html', people=people, chores=chores)
