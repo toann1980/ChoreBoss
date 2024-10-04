db40f7fc78634bb6440491f2669ca5669b793dd5
toann1980 <28768308+toann1980@users.noreply.github.com>
2024-10-03 21:22:18 -0700

    Refactor: Add delete_person_and_adjust_sequence method to PeopleService

--- choreboss/services/people_service.py
+++ choreboss/services/people_service.py
@@ -16,6 +16,17 @@ class PeopleService:
     def delete_person(self, person_id):
         return self.people_repository.delete_person(person_id)
 
+    def delete_person_and_adjust_sequence(self, person_id):
+        person_to_delete = self.get_person_by_id(person_id)
+        if person_to_delete:
+            deleted_person_sequence = person_to_delete.sequence_num
+            self.delete_person(person_id)
+            remaining_people = self.get_all_people_in_sequence_order()
+            for person in remaining_people:
+                if person.sequence_num > deleted_person_sequence:
+                    person.sequence_num -= 1
+                    self.update_person(person)
+
     def get_all_people(self):
         return self.people_repository.get_all_people()
 
