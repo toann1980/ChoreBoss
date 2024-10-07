8ff1c523e9deadea23fd081ccc1569ab78d510bc
toann1980 <28768308+toann1980@users.noreply.github.com>
2024-10-07 11:40:40 -0700

    Refactor: Update home method signature and add docstring

--- web/flask_app/routes/index_routes.py
+++ web/flask_app/routes/index_routes.py
@@ -1,5 +1,5 @@
-from flask import (Blueprint, render_template)
-
+from flask import (Blueprint, render_template, Response)
+from typing import Union
 from choreboss.schemas.chore_schema import ChoreSchema
 from choreboss.schemas.people_schema import PeopleSchema
 from web.flask_app.routes.chore_routes import chore_service
@@ -12,7 +12,13 @@ people_schema = PeopleSchema(many=True)
 
 
 @index_bp.route('/', methods=['GET'])
-def home():
+def home() -> Union[Response, str]:
+    """
+    Render the home page with a list of people and chores.
+
+    Returns:
+        Any: The rendered template for the home page.
+    """
     people = people_service.get_all_people()
     chores = chore_service.get_all_chores()
 
