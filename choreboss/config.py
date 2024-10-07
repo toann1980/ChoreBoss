54924c7884121252d558c5ca3c3e7d0f347d5a3f
toann1980 <28768308+toann1980@users.noreply.github.com>
2024-10-07 11:17:42 -0700

    Refactor: Update SECRET_KEY retrieval in config.py

--- choreboss/config.py
+++ choreboss/config.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URL', 'sqlite:///choreboss.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', '4DunningUey$')
+    SECRET_KEY = os.getenv('SECRET_KEY')
