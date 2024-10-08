8ea4da47206aa0aca06294696c2bf1192b3b7e08
toann1980 <28768308+toann1980@users.noreply.github.com>
2024-10-08 11:44:40 -0700

    Refactor: Update index_routes.py to use current_app for accessing services

--- web/flask_app/routes/index_routes.py
+++ web/flask_app/routes/index_routes.py
@@ -1,14 +1,8 @@
-from flask import (Blueprint, render_template, Response)
+from flask import (Blueprint, current_app, render_template, Response)
 from typing import Union
-from choreboss.schemas.chore_schema import ChoreSchema
-from choreboss.schemas.people_schema import PeopleSchema
-from web.flask_app.routes.chore_routes import chore_service
-from web.flask_app.routes.people_routes import people_service
 
 
 index_bp = Blueprint('index_bp', __name__)
-chore_schema = ChoreSchema(many=True)
-people_schema = PeopleSchema(many=True)
 
 
 @index_bp.route('/', methods=['GET'])
@@ -19,7 +13,7 @@ def home() -> Union[Response, str]:
     Returns:
         Any: The rendered template for the home page.
     """
-    people = people_service.get_all_people()
-    chores = chore_service.get_all_chores()
+    people = current_app.people_service.get_all_people()
+    chores = current_app.chore_service.get_all_chores()
 
     return render_template('index.html', people=people, chores=chores)
